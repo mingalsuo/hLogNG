79bbe40869b39e6d8593201cb8ae3ceeec1be6cd
Markus Ingalsuo <markus.ingalsuo@gmail.com>
2015-02-02 18:44:25 +0200

    Began implementing timing and database connection.

---
+++ src/hLogNG/Program.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Data;
+using Npgsql;
+
+namespace hLogNG
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int interval = 60;
+			while(true)
+			{
+				if (DateTime.Now.ToString("ss") == "00")
+				{
+					Console.WriteLine("Second 00");
+					break;
+				}
+				else
+				{
+					System.Threading.Thread.Sleep(1000);
+				}
+			}
+			while (true)
+			{
+				Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffzz"));
+				System.Threading.Thread.Sleep (1000);
+			}
+			//2004-10-19 10:23:54+02
+			//Console.WriteLine(DateTime.Now() + "\n" + DateTime.Now.ToString("HH:mm:ss") + "\n" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffzz"));
+//			string connectionString =
+//				"Server=127.0.0.1;" +
+//					"Database=DATABASE;" +
+//					"User ID=USER;" +
+//					"Password=PASSWORD;";
+//			IDbConnection dbcon;
+//			dbcon = new NpgsqlConnection(connectionString);
+//			dbcon.Open();
+//			IDbCommand dbcmd = dbcon.CreateCommand();
+//			// requires a table to be created named employee
+//			// with columns firstname and lastname
+//			// such as,
+//			//        CREATE TABLE employee (
+//			//           firstname varchar(32),
+//			//           lastname varchar(32));
+//			string sql =
+//				"SELECT firstname, lastname " +
+//					"FROM employee";
+//			dbcmd.CommandText = sql;
+//			IDataReader reader = dbcmd.ExecuteReader();
+//			while(reader.Read()) {
+//				string FirstName = reader.GetString(reader.GetOrdinal("firstname"));
+//				string LastName = reader.GetString(reader.GetOrdinal("lastname"));
+//				Console.WriteLine("Name: " +
+//				                  FirstName + " " + LastName);
+//			}
+//			// clean up
+//			reader.Close();
+//			reader = null;
+//			dbcmd.Dispose();
+//			dbcmd = null;
+//			dbcon.Close();
+//			dbcon = null;
+		}
+	}
+}
