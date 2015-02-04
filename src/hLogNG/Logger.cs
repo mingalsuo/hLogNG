1be5983e11f2727245592d4c11f7967e353b25d6
Markus Ingalsuo <markus.ingalsuo@gmail.com>
2015-02-04 22:44:45 +0200

    Added method that does SQL-queries and moved stuff around

--- src/hLogNG/Logger.cs
+++ src/hLogNG/Logger.cs
@@ -17,9 +17,41 @@ namespace hLogNG
 			data = parser.ReadFile (IniFile);
 
 			interval = Convert.ToInt32 (data ["MAIN"] ["interval"]);
-			String devicetype = data ["MAIN"] ["devicetype"];
-			String device = data ["MAIN"] ["device"];
-			String script = data ["MAIN"] ["script"];
+			//String devicetype = data ["MAIN"] ["devicetype"];
+			//String device = data ["MAIN"] ["device"];
+			//String script = data ["MAIN"] ["script"];
+		}
+
+		private String sqlQuery (String sql)
+		{
+			string connectionString = String.Format("Server={0};Database={1};User ID={2};Password={3};",
+			data ["MAIN"] ["dbaddr"],
+			data ["MAIN"] ["dbname"],
+			data ["MAIN"] ["dbuser"],
+			data ["MAIN"] ["dbpass"]);
+			Console.WriteLine("connectionString: {0}", connectionString);
+
+			return connectionString;
+//			IDbConnection dbcon;
+//			dbcon = new NpgsqlConnection (connectionString);
+//			dbcon.Open ();
+//			IDbCommand dbcmd = dbcon.CreateCommand ();
+//
+//			dbcmd.CommandText = sql;
+//			IDataReader reader = dbcmd.ExecuteReader ();
+//			while (reader.Read()) {
+//				string FirstName = reader.GetString (reader.GetOrdinal ("firstname"));
+//				string LastName = reader.GetString (reader.GetOrdinal ("lastname"));
+//				Console.WriteLine ("Name: " +
+//					FirstName + " " + LastName);
+//			}
+//			// clean up
+//			reader.Close ();
+//			reader = null;
+//			dbcmd.Dispose ();
+//			dbcmd = null;
+//			dbcon.Close ();
+//			dbcon = null;
 		}
 
 		public void onTimerEvent (object source, ElapsedEventArgs e)
@@ -37,7 +69,7 @@ namespace hLogNG
 			}
 			if (values.Count == objList.Length)
 			{
-				storeValues(objList, values);
+				storeValues(objList, values, e.SignalTime);
 			}
 		}
 		private String readValues(String item)
@@ -69,43 +101,22 @@ namespace hLogNG
 			}
 			return null;
 		}
-		private void storeValues(String[] objList, List<String> values)
+		private void storeValues(String[] objList, List<String> values, DateTime timeStamp)
 		{
 			//2004-10-19 10:23:54+02
 			//Console.WriteLine(DateTime.Now() + "\n" + DateTime.Now.ToString("HH:mm:ss") + "\n" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffzz"));
-			//			string connectionString =
-			//				"Server=127.0.0.1;" +
-			//					"Database=DATABASE;" +
-			//					"User ID=USER;" +
-			//					"Password=PASSWORD;";
-			//			IDbConnection dbcon;
-			//			dbcon = new NpgsqlConnection(connectionString);
-			//			dbcon.Open();
-			//			IDbCommand dbcmd = dbcon.CreateCommand();
-			//			// requires a table to be created named employee
-			//			// with columns firstname and lastname
-			//			// such as,
-			//			//        CREATE TABLE employee (
-			//			//           firstname varchar(32),
-			//			//           lastname varchar(32));
-			//			string sql =
-			//				"SELECT firstname, lastname " +
-			//					"FROM employee";
-			//			dbcmd.CommandText = sql;
-			//			IDataReader reader = dbcmd.ExecuteReader();
-			//			while(reader.Read()) {
-			//				string FirstName = reader.GetString(reader.GetOrdinal("firstname"));
-			//				string LastName = reader.GetString(reader.GetOrdinal("lastname"));
-			//				Console.WriteLine("Name: " +
-			//				                  FirstName + " " + LastName);
-			//			}
-			//			// clean up
-			//			reader.Close();
-			//			reader = null;
-			//			dbcmd.Dispose();
-			//			dbcmd = null;
-			//			dbcon.Close();
-			//			dbcon = null;
+			Console.WriteLine ("Values: {0} Objects: {1}", values.Count, objList.Length);
+
+			int index = 0;
+			while (index < values.Count)
+			{
+				sqlQuery (String.Format("INSERT INTO Values (timestamp, object, value) " +
+				                        "VALUES ({0},{1},{2})",
+				                        timeStamp.ToString ("yyyy-MM-dd HH:mm:ss.fffzz"),
+				                        objList[index],
+				                        values[index]));
+				index++;
+			}
 		}
 	}
 }
